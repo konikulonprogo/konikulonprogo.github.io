6bf89fb76ebbf22464b46918670f3f7fc919eefe
Pabo Bernando <pabobernando69@gmail.com>
2023-01-11 22:04:23 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.7300795201ce71266d5871b07e815123.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a2676973d767c2197bdec5009fc35b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e804d581084f02a85193",
+    "url": "/react-gh-pages/static/css/main.d9446858.chunk.css"
+  },
+  {
+    "revision": "1bc818da24e63494591a",
+    "url": "/react-gh-pages/static/js/2.09acf405.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/react-gh-pages/static/js/2.09acf405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e804d581084f02a85193",
+    "url": "/react-gh-pages/static/js/main.2e029422.chunk.js"
+  },
+  {
+    "revision": "d5f76fbaba6c3a8a125b",
+    "url": "/react-gh-pages/static/js/runtime-main.aa091e63.js"
+  },
+  {
+    "revision": "3b9055cb8a0029ecefa7dfc29cb6642c",
+    "url": "/react-gh-pages/static/media/DAYUNG.3b9055cb.jpeg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/react-gh-pages/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "40f416ca0dddaa23b3be5c56c43cadc5",
+    "url": "/react-gh-pages/static/media/dunia.40f416ca.jpeg"
+  },
+  {
+    "revision": "85b77ddae20c1706fb180602b62a743a",
+    "url": "/react-gh-pages/static/media/fadel.85b77dda.jpeg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/react-gh-pages/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/react-gh-pages/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/react-gh-pages/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/react-gh-pages/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/react-gh-pages/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/react-gh-pages/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/react-gh-pages/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/react-gh-pages/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "97bc0dd0389dcb8947a23da4574da172",
+    "url": "/react-gh-pages/static/media/jinglePOrda.97bc0dd0.jpg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/react-gh-pages/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "d3d684bc7d4288601f64c464dd6a4d4a",
+    "url": "/react-gh-pages/static/media/logoporda.d3d684bc.png"
+  },
+  {
+    "revision": "fa30fa683484528a36ffe66baf2fd661",
+    "url": "/react-gh-pages/static/media/maskotporda.fa30fa68.png"
+  },
+  {
+    "revision": "e47263a254976154cd7442a04c244e1f",
+    "url": "/react-gh-pages/static/media/strukturkoni.e47263a2.png"
+  }
+]);
